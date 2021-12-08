02e9f53f55bd10eee2d1d00b8a6bfc7f0ef724ae
Alireza <alireza.es@gmail.com>
2021-12-08 14:41:20 +0330

    Add Chat Container stories

---
+++ src/components/ChatContainer/ChatContainer.stories.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { ChatContainer } from "./ChatContainer";
+
+export default {
+  title: "Chat Container"
+};
+
+export const LightModeChat = () => (
+  <ChatContainer/>
+);
+
